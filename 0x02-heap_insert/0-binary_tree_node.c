f57fba4de6733584775e88bed76144392922aace
#RodrigoMato00 <3293@holbertonschol.com>
2022-05-05 23:22:16 -0300

    0

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_t *binary_tree_node - creates a binary tree
+ *@parent: parent node
+ *@value: value of the node
+ *Return: pointer to the new node
+**/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    
+}
