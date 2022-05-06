86561f419a264f81eb551701c3b80b9fd7af83e4
Rodrigo <3293@holbertonschool.com>
2022-05-05 23:34:21 -0300

    andando

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef BITREE_H
 #define BITREE_H
 
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -16,8 +18,11 @@ struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 };
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s heap_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif
