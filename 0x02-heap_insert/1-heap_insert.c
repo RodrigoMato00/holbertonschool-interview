07d7159e88e05c7a4d8ecfbfec1f1de32a7be64e
#RodrigoMato00 <3293@holbertonschol.com>
2022-05-05 23:57:34 -0300

    1

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,51 +1,23 @@
-#include "binary_trees.h"
-
+#include"binary_trees.h"
 /**
- *heap_t *heap_insert - inserts a value into a Max Binary Heap
- *@root: pointer to the root of the heap
- *@value: value to be inserted
- *Return: pointer to the new node
+ *heap_insert - function that inserts a value into a Max Binary Heap
+ *@root: double pointer to the root node of the Heap
+ *@value: value store in the node to be inserted
+ *Return: pointer to the inserted node
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new_node;
-	heap_t *parent;
-	heap_t *current;
+	heap_t *new_node = NULL;
 
-	new_node = malloc(sizeof(heap_t));
-	if (new_node == NULL)
+	new_node = malloc(sizeof(*new_node));
 
+	if (!new_node)
 		return (NULL);
+
 	new_node->n = value;
-	new_node->parent = NULL;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	if (*root == NULL)
-	{
-		*root = new_node;
-		return (new_node);
-	}
-	current = *root;
-	parent = NULL;
-	while (current != NULL)
-	{
-		parent = current;
-		if (value > current->n)
+	new_node->parent = *root;
 
-			current = current->right;
-		else
-
-			current = current->left;
-	}
-	if (value > parent->n)
-	{
-		parent->right = new_node;
-		new_node->parent = parent;
-	}
-	else
-	{
-		parent->left = new_node;
-		new_node->parent = parent;
-	}
 	return (new_node);
 }
