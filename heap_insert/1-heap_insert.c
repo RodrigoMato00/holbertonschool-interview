7f5967825362fa671a72ba39c1f9b8bc8dff13c8
Rodrigo Mato <85580712+RodrigoMato00@users.noreply.github.com>
2025-02-11 17:58:39 -0300

    1 add correction

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,24 +1,79 @@
+#include "binary_trees.h"
 
-#include"binary_trees.h"
 /**
- *heap_insert - function that inserts a value into a Max Binary Heap
- *@root: double pointer to the root node of the Heap
- *@value: value store in the node to be inserted
- *Return: pointer to the inserted node
+ * swap_values - Swaps values between two nodes
+ * @a: First node
+ * @b: Second node
+ */
+void swap_values(heap_t *a, heap_t *b)
+{
+    int temp = a->n;
+    a->n = b->n;
+    b->n = temp;
+}
+
+/**
+ * bubble_up - Moves the inserted node to its correct position
+ * @node: Pointer to the inserted node
+ */
+void bubble_up(heap_t *node)
+{
+    while (node->parent && node->n > node->parent->n)
+    {
+        swap_values(node, node->parent);
+        node = node->parent;
+    }
+}
+
+/**
+ * get_insert_parent - Finds the parent where the new node should be inserted
+ * @root: Pointer to the root of the tree
+ * Return: Pointer to the parent node
+ */
+heap_t *get_insert_parent(heap_t *root)
+{
+    heap_t *queue[1024];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+    while (front < rear)
+    {
+        heap_t *current = queue[front++];
+
+        if (!current->left || !current->right)
+            return (current);
+
+        queue[rear++] = current->left;
+        queue[rear++] = current->right;
+    }
+    return (NULL);
+}
+
+/**
+ * heap_insert - Inserts a value into a Max Binary Heap
+ * @root: Double pointer to the root node of the heap
+ * @value: The value to insert
+ * Return: Pointer to the newly inserted node, or NULL on failure
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new_node = NULL;
+    if (!root)
+        return (NULL);
 
-	new_node = malloc(sizeof(*new_node));
+    if (!*root)
+        return (*root = binary_tree_node(NULL, value));
 
-	if (!new_node)
-		return (NULL);
+    heap_t *parent = get_insert_parent(*root);
+    heap_t *new_node = binary_tree_node(parent, value);
 
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	new_node->parent = *root;
+    if (!new_node)
+        return (NULL);
 
-	return (new_node);
+    if (!parent->left)
+        parent->left = new_node;
+    else
+        parent->right = new_node;
+
+    bubble_up(new_node);
+    return (new_node);
 }
